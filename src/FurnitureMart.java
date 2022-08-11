67c070324fe85d293c34b80fcf4c188d6361d7d4
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-08-11 12:16:30 +0530

    Defined the behavior FurnitureItem,and calculate d the discount of 5 percent.And returned the price.

--- src/FurnitureMart.java
+++ src/FurnitureMart.java
@@ -7,7 +7,7 @@ public class FurnitureMart {
     String furnitureUsage;
     double price;
 
-    FurnitureMart(){
+    FurnitureMart() {
 
         int furnitureCode = 5342;
         String furnitureType = "Table";
@@ -15,5 +15,12 @@ public class FurnitureMart {
         String colour = "green";
         String furnitureUsage = "outdoor";
         double price = 300.0;
+    }
+
+    public double FurnitureItem() {
+        double discount = 5.0;
+        if (furnitureUsage.equals("outdoor"))
+            price = price - (price * discount / 100);
+        return price;
     }
 }
