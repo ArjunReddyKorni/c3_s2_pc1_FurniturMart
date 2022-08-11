25b9aaf329af1bf6e740a65311c34a36e3012027
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-08-11 12:05:54 +0530

    Created a no argument constructor,and intilizied the variables with the values.

--- src/FurnitureMart.java
+++ src/FurnitureMart.java
@@ -6,4 +6,14 @@ public class FurnitureMart {
     String colour;
     String furnitureUsage;
     double price;
+
+    FurnitureMart(){
+
+        int furnitureCode = 5342;
+        String furnitureType = "Table";
+        String gradeOfFurniture = "grade1";
+        String colour = "green";
+        String furnitureUsage = "outdoor";
+        double price = 300.0;
+    }
 }
